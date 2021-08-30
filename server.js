612705d639fc2a01ffd6f6a397f214e96a3b661a
Banjer71 <davgio7@gmail.com>
2021-08-30 09:40:56 +0200

    added node-fetch package

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const fetch = require("node-fetch");
+const cors = require("cors");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('ciao')
+app.get("/", (req, res) => {
+  res.send("server is working");
 });
 
 app.post("/", async (req, res) => {
-    const api_key = process.env.API_KEY;
-    const api_url = `https://api.musixmatch.com/ws/1.1/track.search?q_artist=${req.body.artist}&page_size=4&page=1&f_has_lyrics=1&s_track_rating=desc&apikey=${API_KEY}`;
-    const fetch_results = await fetch(api_url);
-    const json = await fetch_results.json();
-    const result = json.message.body.track_list;
-    console.log("dati della ricerca:", result);
-    res.send(result);
-  });
+  const api_key = process.env.API_KEY;
+  const api_url = `https://api.musixmatch.com/ws/1.1/track.search?q_artist=${req.body.artist}&page_size=4&page=1&f_has_lyrics=1&s_track_rating=desc&apikey=${api_key}`;
+  const fetch_results = await fetch(api_url);
+  const json = await fetch_results.json();
+  const result = json.message.body.track_list;
+  console.log("dati della ricerca:", result);
+  res.send(result);
+});
 
-app.listen('3001', (req, res) => {
-    console.log('server started on port 3001')
-})
+app.listen("3001", (req, res) => {
+  console.log("server started on port 3001");
+});
