6ec61255e8bcc871c258e8373305808086635193
Banjer71 <davgio7@gmail.com>
2021-09-23 18:35:07 +0100

    add a post route to receive some data from the client

---
+++ models/lyrics.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const lyricsSchema = new mongoose.Schema(
+  {
+    song: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    writeConcern: {
+      w: "majority",
+      j: true,
+      wtimeout: 1000,
+    },
+  }
+);
+
+const Lyrics = mongoose.model("Lyrics", lyricsSchema);
+
+module.exports = Lyrics;
--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const fetch = require("node-fetch");
 const cors = require("cors");
 require("dotenv").config();
+const Lyrics = require('./models/lyrics');
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(`${process.env.DB_CONNECTION_URL}/lyrcsPage`)
+  .then(() => app.listen(PORT, () => console.log(`server running on ${PORT}`)))
+  .catch((err) => console.log(err.message));
+
+app.get('/', (req, res) => {
+  res.send('hello davide')
+})
+
 app.get("/api/:artist", async (req, res) => {
   const api_key = process.env.API_KEY;
-  const {artist} = req.params
+  const { artist } = req.params;
   const api_url = `https://api.musixmatch.com/ws/1.1/track.search?q_artist=${artist}&page_size=4&page=1&f_has_lyrics=1&s_track_rating=desc&apikey=${api_key}`;
   const fetch_results = await fetch(api_url);
   const json = await fetch_results.json();
@@ -18,6 +32,13 @@ app.get("/api/:artist", async (req, res) => {
   res.send(result);
 });
 
-app.listen("3001", (req, res) => {
-  console.log("server started on port 3001");
+app.get("/post", (req, res) => {
+  res.send("post davide");
+});
+
+app.post("/api/song", async (req, res) => {
+  const songSelected = req.body
+  await songSelected
+  console.log('the song is: ', songSelected)
+  res.send("tune to be saved into the database");
 });
