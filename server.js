075598a4d1f19af067cec1f63522d7c8c2627921
Banjer71 <davgio7@gmail.com>
2021-10-11 09:04:32 +0100

    chagend the pw for the db

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(`${process.env.DB_CONNECTION_URL}`)
+  .then(console.log('db connected'))
   .catch((err) => console.log(err.message));
 
 app.get("/", (req, res) => {
