decb8219ec38ac106f594a0100dda9741455f2a5
Banjer71 <davgio7@gmail.com>
2021-10-16 09:54:34 +0100

    addd cors setting

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const Lyrics = require("./models/lyrics");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: '*'}));
 
 const PORT = process.env.PORT || 5000;
 
