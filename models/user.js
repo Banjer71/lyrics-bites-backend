c26f2633c15c9c6ff37ea6e1bfd82c5e2e20afe7
Banjer71 <davgio7@gmail.com>
2021-11-07 09:38:06 +0000

    created users Schema

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const moongose = require("mongoose");
+
+const userSchema = new moongose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { Type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const User = moongose.model("User", userSchema);
+module.exports = User;
